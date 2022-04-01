8dd4b51c80d4c29758c0379802302f46e1f5498e
BuildTools <unconfigured@null.spigotmc.org>
2022-04-01 16:12:32 +0800

    add phone_no check for /generate

--- routes/otp/otp.js
+++ routes/otp/otp.js
@@ -8,16 +8,24 @@ router.post('/generate', (req, res) => {
     function getRandomInt(max) {
         return Math.floor(Math.random() * max);
     }
+
     let phone_no = req.body.phone_no;
 
-    let otp = ""
-    for (let i = 0; i < 6; i++) {
-        otp += getRandomInt(10).toString()
+    if (!phone_no) {
+        res.status(400).send("No phone_no was provided")
+    } else {
+        let otp = ""
+
+        for (let i = 0; i < 6; i++) {
+            otp += getRandomInt(10).toString()
+        }
+
+        generated_otps[phone_no] = {otp: otp}
+
+        res.send({otp: otp, phone_no: phone_no})
     }
 
-    generated_otps[phone_no] = {otp: otp}
-
-    res.send({otp: otp, phone_no: phone_no})
+    
   });
 
 router.post('/verify', (req, res) => {
